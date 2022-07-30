d526905c057d7fa59127bfde6e6b9761bb1974bb
celsocachola <celsocachola@gmail.com>
2022-07-30 00:00:39 +0000

    Primeiro deploy

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, current_user
+from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.exc import IntegrityError
+import os 
 
 app = Flask("hello")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+db_url = os.environ.get("DATABASE_URL") or "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url.replace("postgres", "postgresql")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "pudim"
 
@@ -32,12 +34,13 @@ class User(UserMixin, db.Model):
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    def check_password_hash(self, password):
+    def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 db.create_all()
 
@@ -46,11 +49,11 @@ def index():
     posts = Post.query.all()
     return render_template("index.html", posts=posts)
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=["GET","POST"])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    if request.method == 'POST':
+    if request.method == "POST":
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
@@ -65,11 +68,11 @@ def register():
             return redirect(url_for('login'))
     return render_template('register.html')
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    if request.method == 'POST':
+    if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
@@ -81,8 +84,22 @@ def login():
 
     return render_template("login.html")
 
-
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/create', methods=["GET", "POST"])
+@login_required
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        body = request.form['body']
+        try:
+            post = Post(title=title, body=body, author=current_user)
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('index'))
+        except IntegrityError:
+            flash("Error on create Post, try again later")
+    return render_template('create.html')
